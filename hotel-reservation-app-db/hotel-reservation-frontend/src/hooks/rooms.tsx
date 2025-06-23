64f66626805051e33fec4a8c09cebd929d6e516d
Giancarlo Paredes <gian_paredes@hotmail.com>
2025-06-23 16:25:20 -0500

    002d

--- hotel-reservation-app-db/hotel-reservation-frontend/src/hooks/rooms.tsx
+++ hotel-reservation-app-db/hotel-reservation-frontend/src/hooks/rooms.tsx
@@ -5,22 +5,22 @@ import { performRequestWithRetry } from "../api/retry";
 import { apiUrl } from "../api/config";
 import { configs } from "../api/configs";
 
-//import * as oauth from 'axios-oauth-client';
-//import axios from 'axios';
+import * as oauth from 'axios-oauth-client';
+import axios from 'axios';
 
 export function useGetRooms() {
   const [rooms, setRooms] = useState<RoomType[]>([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<Error>();
 
-  /*
+  
   const getClientCredentials = oauth.clientCredentials(
     axios.create(),
     "https://bfe41d90-923c-472f-b7d5-13d13da073ad-dev.e1-us-east-azure.choreosts.dev/oauth2/token",
     "IblRACLcYrGU6HAbh20ySpRT93d1",
     "KGCVAnKSzEluG1Rpf98CGbDpwr2N"
   );
-  */
+  
   
   const fetchRooms = async (
     checkIn: string,
@@ -30,8 +30,8 @@ export function useGetRooms() {
     setLoading(true);
 
     // Obtener token de acceso
-    //const auth = await getClientCredentials('');
-    //const accessToken = auth.access_token;
+    const auth = await getClientCredentials('');
+    const accessToken = auth.access_token;
 
     const options = {
       method: "GET",
@@ -40,7 +40,7 @@ export function useGetRooms() {
         checkoutDate: checkOut,
         guestCapacity,
         choreoApiKey333: configs.choreoApiKey,
-        accessToken: "accessToken"
+        accessToken: accessToken
       },
       headers: {
         'Choreo-API-444': `${configs.choreoApiKey}`

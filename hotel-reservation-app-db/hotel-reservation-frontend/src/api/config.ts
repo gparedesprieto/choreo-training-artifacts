e2b50f8fb358f6537b41b5fd39704f521da8e4d5
Giancarlo Paredes <gian_paredes@hotmail.com>
2025-06-23 11:09:29 -0500

    001c

--- hotel-reservation-app-db/hotel-reservation-frontend/src/api/config.ts
+++ hotel-reservation-app-db/hotel-reservation-frontend/src/api/config.ts
@@ -7,3 +7,11 @@ declare global {
 }
 
 export const apiUrl = window?.configs?.apiUrl ? window.configs.apiUrl : "http://localhost:3000/api/reservations";
+
+export const config = {
+  serviceUrl: process.env.CHOREO_HOTEL_RESERVATION_CONNECTION_SERVICEURL || '',
+  consumerKey: process.env.CHOREO_HOTEL_RESERVATION_CONNECTION_CONSUMERKEY || '',
+  consumerSecret: process.env.CHOREO_HOTEL_RESERVATION_CONNECTION_CONSUMERSECRET || '',
+  tokenUrl: process.env.CHOREO_HOTEL_RESERVATION_CONNECTION_TOKENURL || '',
+  choreoApiKey: process.env.CHOREO_HOTEL_RESERVATION_CONNECTION_CHOREOAPIKEY || '',
+};

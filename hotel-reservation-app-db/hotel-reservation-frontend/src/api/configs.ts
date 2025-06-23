bb0b2fd1bb4bf6d00dd703e43fb0913b8652757a
Giancarlo Paredes <gian_paredes@hotmail.com>
2025-06-23 15:51:47 -0500

    002a

--- hotel-reservation-app-db/hotel-reservation-frontend/src/api/configs.ts
+++ hotel-reservation-app-db/hotel-reservation-frontend/src/api/configs.ts
@@ -1,7 +1,7 @@
 export const configs = {
-  serviceUrl: process.env.CHOREO_HOTEL_RESERVATION_CONNECTION_SERVICEURL || '',
-  consumerKey: process.env.CHOREO_HOTEL_RESERVATION_CONNECTION_CONSUMERKEY || '',
-  consumerSecret: process.env.CHOREO_HOTEL_RESERVATION_CONNECTION_CONSUMERSECRET || '',
-  tokenUrl: process.env.CHOREO_HOTEL_RESERVATION_CONNECTION_TOKENURL || '',
-  choreoApiKey: process.env.CHOREO_HOTEL_RESERVATION_CONNECTION_CHOREOAPIKEY || 'x2025-abc',
+  serviceUrl: process.env.CHOREO_HOTEL_RESERVATION_CONNECTION_SERVICEURL || 'https://bfe41d90-923c-472f-b7d5-13d13da073ad-dev.e1-us-east-azure.choreoapis.dev/luxuryhotels/hotel-reservation-service/v1',
+  consumerKey: process.env.CHOREO_HOTEL_RESERVATION_CONNECTION_CONSUMERKEY || 'IblRACLcYrGU6HAbh20ySpRT93d1',
+  consumerSecret: process.env.CHOREO_HOTEL_RESERVATION_CONNECTION_CONSUMERSECRET || 'KGCVAnKSzEluG1Rpf98CGbDpwr2N',
+  tokenUrl: process.env.CHOREO_HOTEL_RESERVATION_CONNECTION_TOKENURL || 'https://bfe41d90-923c-472f-b7d5-13d13da073ad-dev.e1-us-east-azure.choreosts.dev/oauth2/token',
+  choreoApiKey: process.env.CHOREO_HOTEL_RESERVATION_CONNECTION_CHOREOAPIKEY || 'chk_eyJjb25uZWN0aW9uLWlkIjoiMDFmMDRiN2QtYzQ5Yi0xNjk4LTk3MDgtM2NmN2JhZTA0OTgxIn0=n9cxIw',
 };
--- hotel-reservation-app-db/hotel-reservation-frontend/src/hooks/rooms.tsx
+++ hotel-reservation-app-db/hotel-reservation-frontend/src/hooks/rooms.tsx
@@ -5,24 +5,42 @@ import { performRequestWithRetry } from "../api/retry";
 import { apiUrl } from "../api/config";
 import { configs } from "../api/configs";
 
+import * as oauth from 'axios-oauth-client';
+import axios from 'axios';
+
 export function useGetRooms() {
   const [rooms, setRooms] = useState<RoomType[]>([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<Error>();
 
+  const getClientCredentials = oauth.clientCredentials(
+    axios.create(),
+    "https://bfe41d90-923c-472f-b7d5-13d13da073ad-dev.e1-us-east-azure.choreosts.dev/oauth2/token",
+    "IblRACLcYrGU6HAbh20ySpRT93d1",
+    "KGCVAnKSzEluG1Rpf98CGbDpwr2N"
+  );
+  
   const fetchRooms = async (
     checkIn: string,
     checkOut: string,
     guestCapacity: number,
   ): Promise<void> => {
     setLoading(true);
+
+
+    // Obtener token de acceso
+    const auth = await getClientCredentials('');
+    const accessToken = auth.access_token;
+
+
     const options = {
       method: "GET",
       params: {
         checkinDate: checkIn,
         checkoutDate: checkOut,
         guestCapacity,
-        choreoApiKey3: configs.choreoApiKey
+        choreoApiKey3: configs.choreoApiKey,
+        accessToken: accessToken
       },
       headers: {
         'Choreo-API-Key4': `${configs.choreoApiKey}`

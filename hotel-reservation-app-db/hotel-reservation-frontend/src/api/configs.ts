44cc57d9a5e827bf04aa96be4756552b2e2d783f
Giancarlo Paredes <gian_paredes@hotmail.com>
2025-06-25 11:18:53 -0500

    002b

--- hotel-reservation-app-db/hotel-reservation-frontend/src/api/configs.ts
+++ hotel-reservation-app-db/hotel-reservation-frontend/src/api/configs.ts
@@ -1,7 +1,7 @@
 export const configs = {
-  serviceUrl: window?.configs?.serviceurl ? window.configs.serviceurl : "",
-  consumerKey: window?.configs?.consumerKey ? window.configs.consumerKey : "",
-  consumerSecret: window?.configs?.consumerSecret ? window.configs.consumerSecret : "",
-  tokenUrl: window?.configs?.tokenUrl ? window.configs.tokenUrl : "",
-  choreoApiKey: window?.configs?.choreoApiKey ? window.configs.choreoApiKey : "",
+  serviceUrl: "https://bfe41d90-923c-472f-b7d5-13d13da073ad-dev.e1-us-east-azure.choreoapis.dev/luxuryhotels/hotel-reservation-service/v1",
+  consumerKey:  "IblRACLcYrGU6HAbh20ySpRT93d1",
+  consumerSecret: "KGCVAnKSzEluG1Rpf98CGbDpwr2N",
+  tokenUrl: 'https://bfe41d90-923c-472f-b7d5-13d13da073ad-dev.e1-us-east-azure.choreosts.dev/oauth2/token',
+  choreoApiKey: "chk_eyJjb25uZWN0aW9uLWlkIjoiMDFmMDRiN2QtYzQ5Yi0xNjk4LTk3MDgtM2NmN2JhZTA0OTgxIn0=n9cxIw"
 };
